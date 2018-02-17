0a8267b1c4edb3a0ba882c74f470dd91653bb1c3
hohanloong <1198432354@qq.com>
2018-02-17 17:12:39 +0800

    遮挡效果 多边形偏移  就是绘制时 对深度值的计算  进行扰动  获得正确的 遮挡效果 深度偏移值 = m * factor + r * units m是多边形深度最大斜率 斜率项 deltaZ/deltaX deltaZ/deltaY代表 z在x，y方向上的斜率; 在三角形 光栅化阶段 由OpenGL ES管线 实现 r是最小分辨 是OpenGL实现的常量 由具体实现而定 比如 深度缓存是16bit的 那么 r=1 就代表 1/65536  2^16=65536

--- Sample5_15_多边形偏移_z深度冲突/app/src/main/java/com/bn/Sample5_15/MySurfaceView.java
+++ Sample5_15_多边形偏移_z深度冲突/app/src/main/java/com/bn/Sample5_15/MySurfaceView.java
@@ -11,11 +11,17 @@ import android.view.MotionEvent;
 class MySurfaceView extends GLSurfaceView 
 {
 	private final float TOUCH_SCALE_FACTOR = 180.0f/320;//角度缩放比例
+
+
     private SceneRenderer mRenderer;//场景渲染器
+
+
     private float mPreviousX;//上次的触控位置X坐标
-    float yAngle=90;//总场景绕y轴旋转的角度
+    float yAngle=0;//总场景绕y轴旋转的角度
     private float mPreviousY;//上次的触控位置X坐标
-    float xAngle=20;//总场景绕y轴旋转的角度
+    float xAngle=0;//总场景绕y轴旋转的角度
+
+
     float polygonOffsetFactor =-1.0f;
     float polygonOffsetUnits  =-2.0f;
     
@@ -62,14 +68,30 @@ class MySurfaceView extends GLSurfaceView
             MatrixState.rotate(xAngle, 1, 0, 0);//绕X轴旋转xAngle度
             //绘制左侧立方体
             MatrixState.pushMatrix();
-            MatrixState.translate(-250f, 0, 0f);// z = -0.1f
+            MatrixState.translate(-250f, 0, -0.5f);// z = -0.1f   必须有这个 才会有效果
+            // 先画蓝色  在画黄色
+
+            // z = +2.0  蓝色一大块  黄色一小块
+            // z = +0.1  蓝色一大块  黄色一小块 没有问题
+            // z = 0 ,   ?? 虽然先渲染蓝色  再渲染黄色的  但是还是蓝色的占一大块??
+            // z = -0.1  !!!  蓝色一大块  黄色一小块  重叠区域出现 条纹  !!!
+            // z = -0.5f  !!!  重叠区域 条纹 特别严重 !!!
+            // z = -2.0 蓝色一小块  黄色一大块
             color1.drawSelf();
             MatrixState.popMatrix();
 
             //!!!! 启用多边形偏移
-//            GLES30.glEnable (GLES30.GL_POLYGON_OFFSET_FILL );
-//            GLES30.glPolygonOffset ( polygonOffsetFactor, polygonOffsetUnits );
-            
+            GLES30.glEnable (GLES30.GL_POLYGON_OFFSET_FILL );
+            GLES30.glPolygonOffset ( polygonOffsetFactor, polygonOffsetUnits );
+            // !!! factor 计算深度值得比例  units 计算深度值的单位 !!!!
+            // 深度偏移值 = m * factor + r * units
+            //
+            // 多边形偏移  就是绘制时 对深度值的计算  进行扰动  获得正确的 遮挡效果
+
+            // m 是三角形  的 最大深度斜率  m=sqrt( (deltaZ/deltaX)^2 + (deltaZ/deltaY)^2 )
+            //                  斜率项 deltaZ/deltaX deltaZ/deltaY z在x，y方向上的斜率 在三角形 光栅化阶段 由OpenGL ES管线实现
+            // r 是 OpenGL ES 实现中定义的常量，代表深度值中可以保证产生差异的最小值
+
             //绘制右侧立方体
             MatrixState.pushMatrix();
             MatrixState.translate(250f,0, 0f);
@@ -95,7 +117,8 @@ class MySurfaceView extends GLSurfaceView
 
 	        // 透视投影矩阵 和  摄像机矩阵
 	        MatrixState.setProjectFrustum(-ratio*75f, ratio*75f, -75f, 75f, 300f, 10000f ); // !!! 视椎体 达到  300~10000f  两个矩形位于 5000f
-	        MatrixState.setCamera(5000f,0.5f,0f,   0f,0f,0f,    0f,1.0f,0.0f);
+	        MatrixState.setCamera( 0f ,0.5f ,5000f ,/* 摄像机位于z轴方向上*/
+	                                0f,0f,0f,    0f,1.0f,0.0f);
 
 
 	        
@@ -112,13 +135,13 @@ class MySurfaceView extends GLSurfaceView
 
 
             //创建 两个矩形 对象   600.0f x 600.0f   在世界坐标系中-250   250
-            color1=new ColorRect(MySurfaceView.this,new float[]{0,0,1,0});//蓝色
-            color2=new ColorRect(MySurfaceView.this,new float[]{1,0,0,0});//黄色
+            color1=new ColorRect(MySurfaceView.this,new float[]{0,1,1,0});//蓝色
+            color2=new ColorRect(MySurfaceView.this,new float[]{1,1,0,0});//黄色
 
 
             // 深度检测  背面剪裁
             GLES30.glEnable(GLES30.GL_DEPTH_TEST);
-            //GLES30.glEnable(GLES30.GL_CULL_FACE); // !!!  hhl   这样旋转可以看到背面
+            GLES30.glEnable(GLES30.GL_CULL_FACE); // !!!  hhl   这样旋转可以看到背面
         }
     }
 }
