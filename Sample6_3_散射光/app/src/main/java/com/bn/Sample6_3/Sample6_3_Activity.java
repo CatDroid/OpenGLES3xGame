1c513e8de4115e5de47ef605e35578348ddf8bbc
hohanloong <1198432354@qq.com>
2018-02-25 00:24:32 +0800

    散射光模型 1.反射光是均匀的  只跟光源位置有关 2.散射光最终效果=材质的反射系数 * 散射光最终强度  散射光最终强度=散射光强度*max(0,cos(光源与法向量夹角)), 夹角大于90度 为0 3.由于从模型坐标系到世界坐标系  旋转(正交矩阵)和位移(不影响法向量) 所以可以简化为 模型变换矩阵*当前法向量

---
+++ Sample6_3_散射光/app/src/main/java/com/bn/Sample6_3/Sample6_3_Activity.java
@@ -0,0 +1,55 @@
+package com.bn.Sample6_3;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+public class Sample6_3_Activity extends Activity {
+	private MySurfaceView mGLSurfaceView;
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 设置为全屏
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//设置为横屏
+		// 初始化GLSurfaceView
+		mGLSurfaceView = new MySurfaceView(this);
+		// 切换到主界面
+		setContentView(R.layout.main);	
+		LinearLayout ll = (LinearLayout) findViewById(R.id.main_liner);
+		ll.addView(mGLSurfaceView);
+        //普通拖拉条被拉动的处理代码
+        SeekBar sb=(SeekBar)this.findViewById(R.id.SeekBar01);
+        sb.setOnSeekBarChangeListener(
+            new SeekBar.OnSeekBarChangeListener()
+            {
+				@Override
+				public void onProgressChanged(SeekBar seekBar, int progress,
+						boolean fromUser) {
+					mGLSurfaceView.setLightOffset((seekBar.getMax()/2.0f-progress)/(seekBar.getMax()/2.0f)*-4);
+				}
+				@Override
+				public void onStartTrackingTouch(SeekBar seekBar) {	}
+				@Override
+				public void onStopTrackingTouch(SeekBar seekBar) { }            	
+            }
+        );
+	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause(); 
+    } 
+}
